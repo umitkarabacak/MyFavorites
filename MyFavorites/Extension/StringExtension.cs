cf8cd17ddf8d7e13a77daec49daa4a83b0103aab
ÜMİT KARABACAK <umit.karabacak@hotmail.com.tr>
2020-11-19 00:46:55 +0300

    Added string extension

---
+++ MyFavorites/Extension/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MyFavorites.Extension
+{
+    public static class StringExtension
+    {
+        public static string RemoveSpaces(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty;
+
+            return text.Trim().Replace(" ", string.Empty);
+        }
+
+        public static string DecimalBraces(this string text)
+        {
+            return text.Trim().Replace(",", ".");
+        }
+
+        public static bool IsValidMailAddress(this string text)
+            => Regex.IsMatch(text, @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+
+        public static string CustomFormat(this string text, params object[] args)
+        {
+            return string.Format(text, args);
+        }
+    }
+}
